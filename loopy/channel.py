35caeba456165cc944980772806501d3e5d15970
unknown <xl3133@nyu.edu>
2023-01-24 17:20:53 +0800

    ready_for_template_arrangement

--- loopy/channel.py
+++ loopy/channel.py
@@ -4,11 +4,11 @@ import numpy as np
 class LoopyChannel():
     def __init__(self,
         channel_id,
-        sr: int = 44100,
+        # sr: int = 44100,
         name: str = None,
     ) -> None:
         self._channel_id = channel_id
-        self._sr = sr
+        # self._sr = sr
         self._name = name
         self._effects = []  # list of LoopyEffect
         
